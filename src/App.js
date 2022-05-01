6d45f165cddf6902e55e4a9cc2cdb104d723bc11
renanlimaleite <renan.lima.softwares@gmail.com>
2022-05-01 18:05:57 -0300

    feat: add global styles with provider from styled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
+import { ThemeProvider } from 'styled-components'
+import defaultTheme from './assets/themes/default'
+import GlobalStyles from './assets/styles/global'
+
 function App () {
   return (
-    <h1>Oi</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyles />
+      <h1>Oi</h1>
+    </ThemeProvider>
   )
 }
 
