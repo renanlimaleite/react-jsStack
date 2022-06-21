742857cc3b8090fdecfdb9100a9c90636c733938
renanlimaleite <renan.lima.softwares@gmail.com>
2022-06-21 15:26:08 -0300

    add make request abstraction in httpClient

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -1,5 +1,32 @@
 import { delay } from '../../utils/delay'
 
+// eslint-disable-next-line no-unused-vars
+const makeRequest = async (baseURL, path, options) => {
+  await delay(500)
+
+  const headers = new Headers({
+    'Content-Type': 'application/json'
+  })
+
+  const response = await fetch(`${baseURL}${path}`, {
+    method: options.method,
+    body: JSON.stringify(options.body),
+    headers
+  })
+
+  let responseBody = null
+  const contentType = response.headers.get('Content-Type')
+  if (contentType.includes('application/json')) {
+    responseBody = await response.json()
+  }
+
+  if (response.ok) {
+    return responseBody
+  }
+
+  return response
+}
+
 export const BaseHttpClient = (baseURL) => {
   return {
     get: async (path) => {
